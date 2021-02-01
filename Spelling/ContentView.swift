5ea25a7b7e4b299ee7ecf48f8adc86cb79ffa667
Denis Roenko <denis@roenko.ru>
2021-02-01 23:11:28 +0300

    Initial Commit

---
+++ Spelling/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Spelling
+//
+//  Created by  g01dt00th on 01.02.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Spelling/SpellingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpellingApp.swift
+//  Spelling
+//
+//  Created by  g01dt00th on 01.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct SpellingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
